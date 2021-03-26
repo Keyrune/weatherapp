21eafa4bc89e9772927fc4db9ff5b2d148692aa1
Keyrune <spyro-99@mail.ru>
2021-03-26 19:10:45 +0300

    main page with weather

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() => runApp(MaterialApp(title: "Wheather App", home: Home()));
 
@@ -12,6 +15,67 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Column(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height / 3,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.red,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Text("Москва",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600)),
+              ),
+              Text('13\u00b0',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w600)),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Text("Погода",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600)),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                    title: Text('температура'),
+                    trailing: Text('13\u00b0')),
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text('weather'),
+                    trailing: Text('weather')),
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text('солнышко'),
+                    trailing: Text('13')),
+                ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text('скорость ветра'),
+                    trailing: Text('13')),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
